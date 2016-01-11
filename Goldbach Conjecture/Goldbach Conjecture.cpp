0ac4175291beb64101ebfec289090c4efaae239c
Shubho <shubho.aiub@gmail.com>
2016-01-12 01:35:28 +0600

    Goldbach Conjecture with cpp

--- Goldbach Conjecture/Goldbach Conjecture.cpp
+++ Goldbach Conjecture/Goldbach Conjecture.cpp
@@ -6,9 +6,12 @@ using namespace std;
  int main(){
     vector<int>v1;
     input:
-    cout<<"input an even integer (>4) : ";int l;cin>>l;
+    cout<<"input an even integer (>4) : ";
+    int l;cin>>l;
+
     if(l<=4 || l%2!=0)
         goto input;
+
     for(int i=3; i<l; i=i+2){int flag=1;
         for(int j=2;j<i; j++)
             if(i%j==0){
@@ -16,9 +19,11 @@ using namespace std;
         if(flag)
             v1.push_back(i);
     }
+
    for(vector<int>::iterator i=v1.begin();i<v1.end(); i++)
         cout<<*i<<" ";
         cout<<endl;
+
     for(int i=0; i!=v1.size(); i++)
          for(int j=v1.size(); j!=-1; j--)
             if(v1[i]+v1[j]==l ){
